fe26d19ecb1c985e2fa0644c3c26dc7e7698e678
AJLAN0 <aj0ly.hd@gmail.com>
2025-07-24 04:48:52 +0300

    best version

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import Admin from './pages/Admin';
 const App: React.FC = () => (
   <AuthProvider>
     <CMSProvider>
-      <Router basename="/">
+      <Router>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
