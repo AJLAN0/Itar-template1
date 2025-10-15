bea21bef5e6799edaebd1b0f44551378dd16c529
AJLAN <107946506+AJLAN0@users.noreply.github.com>
2025-10-15 15:55:19 +0300

    Wrap routes in AuthProvider and add login route

--- src/App.tsx
+++ src/App.tsx
@@ -8,21 +8,24 @@ import Login from './pages/Login';
 import Admin from './pages/Admin';
 
 const App: React.FC = () => (
-
+<AuthProvidor>
     <CMSProvider>
       <Router>
         <Routes>
           <Route path="" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <ProtectedRoute>
           <Route
             path="/admin"
             element={
                 <Admin />
             }
           />
+                </ProtectedRoute>
         </Routes>
       </Router>
     </CMSProvider>
-
+</AuthProvidor>
 );
 
 export default App;
